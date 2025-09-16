c487726c66dd659734f9b1742013f88e219135cb
AnnaMatilda-IT <annaeliseeva86@mail.ru>
2025-09-17 01:17:19 +0500

    fix1

--- src/main/java/web/service/CarService.java
+++ src/main/java/web/service/CarService.java
@@ -1,30 +1,20 @@
 package web.service;
 
 import org.springframework.stereotype.Service;
+import web.dao.CarDao;
+import web.dao.CarDaoImpl;
 import web.model.Car;
-
-import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class CarService {
-    private List<Car> cars;
+    private CarDao carDao;
 
-    public CarService() {
-        // Инициализация списка из 5 машин
-        cars = Arrays.asList(
-                new Car("Toyota", "Camry", 2020),
-                new Car("Honda", "Civic", 2021),
-                new Car("BMW", "X5", 2019),
-                new Car("Mercedes", "C-Class", 2022),
-                new Car("Audi", "A4", 2018)
-        );
+    public CarService(CarDao carDao) {
+        this.carDao = new CarDaoImpl();
     }
 
     public List<Car> getCars(int count) {
-        if (count >= 5) {
-            return cars;
-        }
-        return cars.subList(0, count);
+        return carDao.getByCount(count);
     }
 }
